05231adb34c452bef437d2506339008d2ba37342
Bengt Lüers <bengt.lueers@gmail.com>
2023-08-27 00:32:17 +0200

    Define FUSS root directoy

--- models/neurips2020_mixit/setup.sh
+++ models/neurips2020_mixit/setup.sh
@@ -17,6 +17,7 @@
 # Uncomment next line and change to the directory for storing model data.
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 ROOT_DIR="$SCRIPT_PATH/data"
+FUSS_ROOT_DIR="$SCRIPT_PATH/../../datasets/fuss"
 
 if [ x"${ROOT_DIR}" == x ]; then
   echo "Please define ROOT_DIR variable inside `dirname $0`/setup.sh."
