d2b8c06fb265ad9231ced085af00eafc63c10c2f
Bengt Lüers <bengt.lueers@gmail.com>
2023-11-06 00:35:48 +0100

    Add newline at EOF

--- tests/_paths.py
+++ tests/_paths.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 ROOT_PATH: Path = \
     Path('/ds') if Path('/ds').exists() else \
-    Path(__file__).parent.parent.parent / '5-models'
+    Path(__file__).parent.parent.parent
