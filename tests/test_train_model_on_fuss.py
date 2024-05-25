8a06aee275548add02d6289998074f32e2122df0
Bengt Lüers <bengt.lueers@gmail.com>
2024-05-25 03:21:28 +0200

    Fix test

--- tests/test_train_model_on_fuss.py
+++ tests/test_train_model_on_fuss.py
@@ -10,49 +10,49 @@ from tests._paths import ROOT_PATH
 @pytest.mark.parametrize(
     ('dataset_name', 'dataset_path'),
     [
-        ('fuss', 'fuss/fuss_dev/ssdata'),
-        ('anuraset', 'AnuraSet_Dev/anuraset_dev/ssdata'),
-        ('rfcx', 'xprize.rfcx_dev/'),
+        # ('fuss', 'fuss/fuss_dev/ssdata'),
+        # ('anuraset', 'AnuraSet_Dev/anuraset_dev/ssdata'),
+        # ('rfcx', 'xprize.rfcx_dev/'),
         # Fails:
         # ('congo_soundscapes_dev', 'congo_soundscapes_dev/10_random_examples'),
+        ('xprize', Path('/media/bengt/XPRIZE-1TB/08-mixes')),
     ],
 )
 def test_train_model_on_fuss(dataset_name, dataset_path):
     # Configuration
-    line_limit = 4
-    train_steps = 4
+    line_limit = 10
+    train_steps = 10
 
     # Arrange
-    root_data_audio_path: Path = ROOT_PATH / '02-datasets' / 'audio'
     train_example_file_path = (
-            root_data_audio_path / dataset_path / 'train_example_list.txt'
+            dataset_path / 'train_example_list.txt'
     )
     test_train_example_file_path = (
-            root_data_audio_path / dataset_path / 'test_train_example_list.txt'
+            dataset_path / 'test_train_example_list.txt'
     )
     _copy_first_lines(
         input_file_path=train_example_file_path,
         output_file_path=test_train_example_file_path,
-        line_limit=line_limit,
+        line_limit=line_limit + 2,
     )
 
     validation_example_file_path = (
-            root_data_audio_path / dataset_path / 'validation_example_list.txt'
+            dataset_path / 'validation_example_list.txt'
     )
     test_evaluation_example_file_path = (
-            root_data_audio_path / dataset_path / 'test_validation_example_list.txt'
+            dataset_path / 'test_validation_example_list.txt'
     )
     _copy_first_lines(
         input_file_path=validation_example_file_path,
         output_file_path=test_evaluation_example_file_path,
-        line_limit=line_limit,
+        line_limit=line_limit + 2,
     )
 
     test_validation_list_file_path_string = str(
-        root_data_audio_path / dataset_path / 'test_validation_example_list.txt'
+        dataset_path / 'test_validation_example_list.txt'
     )
     test_train_list_file_path_string = str(
-        root_data_audio_path / dataset_path / 'test_train_example_list.txt'
+        dataset_path / 'test_train_example_list.txt'
     )
     datetime_slug = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     datetime_slug += '-test'
@@ -67,6 +67,7 @@ def test_train_model_on_fuss(dataset_name, dataset_path):
         validation_list=test_validation_list_file_path_string,
         model_dir=model_dir,
         train_steps=train_steps,
+        batch_size_half=10,
     )
 
 
